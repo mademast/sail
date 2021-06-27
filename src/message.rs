b9e558492a0f9d015f7edb49bd6442af6bb3ac5b
Devon Sawatsky <novedevo@gmail.com>
2021-06-27 14:02:33 -0700

    add undeliverable message generator

--- src/message.rs
+++ src/message.rs
@@ -5,3 +5,13 @@ pub struct Message {
     pub forward_paths: Vec<String>,
     pub data: Vec<String>,
 }
+
+impl Message {
+    pub fn undeliverable(reasons: Vec<String>, reverse_path: &str) -> Self {
+        Self {
+            reverse_path: "<>".to_string(),
+            forward_paths: vec![reverse_path.to_string()],
+            data: reasons,
+        }
+    }
+}
