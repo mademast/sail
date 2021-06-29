d0a21c09ce536aba9eba3d470a70f854d18dd749
Devon Sawatsky <novedevo@gmail.com>
2021-06-28 18:40:55 -0700

    Implement Display for Path

--- src/path.rs
+++ src/path.rs
@@ -1,7 +1,41 @@
+use std::fmt::{Display, Formatter, Result};
+
 use crate::domain::Domain;
 
+struct Path {
+	local_part: String,
+	domain: Domain,
+}
 
-pub struct Path {
-    local_part: Option<String>,
-    domain: Option<Domain>,
+pub enum ForwardPath {
+	Postmaster,
+	Regular(Path),
+}
+
+pub enum ReversePath {
+	Null,
+	Regular(Path),
+}
+
+impl Display for Path {
+	fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+		write!(f, "<{}@{}>", self.local_part, self.domain)
+	}
+}
+
+impl Display for ForwardPath {
+	fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+		match self {
+			Self::Postmaster => write!(f, "<postmaster>"),
+			Self::Regular(path) => write!(f, "{}", path),
+		}
+	}
+}
+impl Display for ReversePath {
+	fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+		match self {
+			Self::Null => write!(f, "<>"),
+			Self::Regular(path) => write!(f, "{}", path),
+		}
+	}
 }
