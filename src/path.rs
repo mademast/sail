37961c9262873216bb211bf14ece82af921c5b93
Devon Sawatsky <novedevo@gmail.com>
2021-06-28 21:04:30 -0700

    genbyte: add ADL stripping abilities

--- src/path.rs
+++ src/path.rs
@@ -45,7 +45,6 @@ impl Display for ReversePath {
 		}
 	}
 }
-
 impl FromStr for Path {
 	type Err = ParsePathError;
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -53,23 +52,14 @@ impl FromStr for Path {
 			if let Some(stripped) = path.strip_suffix(">") {
 				if !stripped.starts_with('@') {
 					// ADLs have to start with @
-					let splits = stripped.rsplit_once("@");
-
-					if let Some((local_part, domain)) = splits {
-						// Check if it's an address literal first, and if it isn't, check if it's a domain
-						if ArgParser::validate_local_part(local_part) {
-							Ok(Self {
-								local_part: local_part.to_string(),
-								domain: Domain::from_str(domain)?,
-							})
-						} else {
-							Err(ParsePathError::InvalidLocalPart)
-						}
-					} else {
-						Err(ParsePathError::NoAtSign)
-					}
+					Self::parse_naked_path(stripped)
 				} else {
-					todo!() //todo: ADLs
+					let splits: Vec<&str> = stripped.splitn(2, ':').collect();
+					if splits.len() != 2 {
+						Err(ParsePathError::AdlWithoutColon)
+					} else {
+						Self::parse_naked_path(splits[1])
+					}
 				}
 			} else {
 				Err(ParsePathError::Brackets)
@@ -80,12 +70,34 @@ impl FromStr for Path {
 	}
 }
 
+impl Path {
+	fn parse_naked_path(stripped: &str) -> Result<Self, ParsePathError> {
+		let splits = stripped.rsplit_once("@");
+
+		if let Some((local_part, domain)) = splits {
+			// Check if it's an address literal first, and if it isn't, check if it's a domain
+			if ArgParser::validate_local_part(local_part) {
+				Ok(Self {
+					local_part: local_part.to_string(),
+					domain: Domain::from_str(domain)?,
+				})
+			} else {
+				Err(ParsePathError::InvalidLocalPart)
+			}
+		} else {
+			Err(ParsePathError::NoAtSign)
+		}
+	}
+}
+
 #[derive(Error, Debug)]
 pub enum ParsePathError {
 	#[error("no enclosing angle brackets")]
 	Brackets,
 	#[error("no @")]
 	NoAtSign,
+	#[error("ADL syntax without colon")]
+	AdlWithoutColon,
 	#[error("invalid local part")]
 	InvalidLocalPart,
 	#[error("invalid domain")]
