b366f1b51319e1397e0fd055943c05dc6b7bb01e
Devon Sawatsky <novedevo@gmail.com>
2021-09-29 22:33:00 -0700

    add integration test

--- src/smtp/mod.rs
+++ src/smtp/mod.rs
@@ -10,3 +10,37 @@ pub use command::Command;
 pub use message::Message;
 pub use response::{Response, ResponseCode};
 pub use server::Server;
+
+mod test {
+
+	#[test]
+	#[ignore] //only run in CI contexts
+	fn send_trigger() { //sends a message to an email set by us
+		use std::str::FromStr;
+
+		use super::{
+			super::net,
+			args::{Domain, Path, ReversePath},
+			ForeignMessage, ForeignPath,
+		};
+		let forward_paths = vec![ForeignPath(
+			Path::from_str(&format!("<{}>", std::env::var("TRIGGER_EMAIL").unwrap())).unwrap(),
+		)];
+		let reverse_path = ReversePath::Null;
+		let data = "".to_string();
+
+		let message = ForeignMessage {
+			forward_paths,
+			reverse_path,
+			data,
+		};
+
+		let future = net::relay(Domain::from_str("oracle.nove.dev").unwrap(), message);
+
+		tokio::runtime::Builder::new_current_thread()
+			.enable_all()
+			.build()
+			.unwrap()
+			.block_on(future);
+	}
+}
